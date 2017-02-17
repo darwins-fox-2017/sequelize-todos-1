876ce6384fbc24fe219a254dfbee0b7836ad307f
wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-02-17 18:20:40 +0700

    Daniel Sequelize-Todo

---
+++ migrations/20170217040859-create-todo.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      task: {
+        type: Sequelize.STRING
+      },
+      completed: {
+        type: Sequelize.BOOLEAN
+      },
+      tags: {
+        type: Sequelize.STRING
+      },
+      completedAt: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Todos');
+  }
+};
---
+++ models/todo.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Todo = sequelize.define('Todo', {
+    task: DataTypes.STRING,
+    completed: DataTypes.BOOLEAN,
+    tags: DataTypes.STRING,
+    completedAt: DataTypes.DATE
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Todo;
+};
